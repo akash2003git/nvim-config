d986844aaa9317edc436ce23d91252cdfd0696a6
akash2003git <akash.tayade.dev@gmail.com>
2024-12-29 19:27:12 +0530

    Updates

--- lua/akash/plugins/lsp/lspconfig.lua
+++ lua/akash/plugins/lsp/lspconfig.lua
@@ -120,7 +120,7 @@ return {
         -- configure emmet language server
         lspconfig["emmet_ls"].setup({
           capabilities = capabilities,
-          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+          filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte", "ejs" },
         })
       end,
       ["clangd"] = function()

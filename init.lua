0abc1d9a67f29cfa838b1e0837d1688fa7d4e82a
akash2003git <akash.tayade.dev@gmail.com>
2025-02-26 12:52:59 +0530

    dsa setup

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 require("akash.core")
 require("akash.lazy")
+
+-- makeshift dsa practice setup
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>rr",
+  ":!> ~/Coding/a2z_dsa/output.txt && g++ % -o temp && ./temp < ~/Coding/a2z_dsa/input.txt > ~/Coding/a2z_dsa/output.txt<CR>",
+  { noremap = true, silent = true }
+)

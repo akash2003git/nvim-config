615642543e33e6a4e910fe656ded1ad2b4a41782
akash2003git <akash.tayade.dev@gmail.com>
2025-05-04 14:01:35 +0530

    Updates

--- lua/akash/core/keymaps.lua
+++ lua/akash/core/keymaps.lua
@@ -22,8 +22,6 @@ keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
-keymap.set("n", "<Esc>", ":nohlsearch<CR>", { noremap = true, silent = true }) -- remove search highlights
-
 keymap.set(
   "n",
   "<leader>rr",

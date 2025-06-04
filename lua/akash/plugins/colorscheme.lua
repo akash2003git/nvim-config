c9801f5080088a1ea5edc33f593b272dbed47c48
akash2003git <akash.tayade.dev@gmail.com>
2025-06-04 13:53:03 +0530

    Transparency

--- lua/akash/plugins/colorscheme.lua
+++ lua/akash/plugins/colorscheme.lua
@@ -3,6 +3,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end
+    require("catppuccin").setup({
+      flavour = "mocha", -- You can change this to 'latte', 'frappe', or 'mocha' if you prefer
+      transparent_background = true, -- This is the key line for transparency
+      -- Other Catppuccin options can go here if you have any, for example:
+      -- term_colors = true,
+      -- no_italic = true,
+    })
+    vim.cmd.colorscheme("catppuccin")
+  end,
 }
